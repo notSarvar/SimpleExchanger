8c999f7a7ea16c48d41e1217d0bd692b9d609490
Sarvar Yuldashev <sarvar010104@gmail.com>
2024-10-04 21:30:51 +0300

    Some fixes

--- Core.h
+++ Core.h
@@ -4,10 +4,10 @@
 #include "Orders.cpp"
 #include "Singleton.hpp"
 #include "User.h"
-#include "json.hpp"
+// #include "json.hpp"
 
 #include <unordered_map>
-#include <unordered_set>
+// #include <unordered_set>
 
 class Core : public Singleton<Core> {
 public:
@@ -16,9 +16,9 @@ public:
                                 size_t quantity) {
       const Order &our_order = order_book_.get_order_by_id(our_order_id);
       const Order &other_order = order_book_.get_order_by_id(other_order_id);
-      to_send_[other_order.user_id].push_back(other_order.as_json());
+      // to_send_[other_order.user_id].push_back(other_order.as_json());
       if (our_order.user_id != other_order.user_id) {
-        to_notify_.insert(other_order.user_id);
+        // to_notify_.insert(other_order.user_id);
         if constexpr (side == ESide::ESell) {
           users_[other_order.user_id].add_usd(quantity);
           users_[other_order.user_id].add_rub(-quantity * other_order.price);
@@ -36,7 +36,6 @@ public:
         order_book_.addOrder(side, trade_handler, std::forward<Args>(args)...);
     const Order &order = order_book_.get_order_by_id(order_id);
     users_[order.user_id].add_order(order_id);
-    to_send_[order.user_id].push_back(order.as_json());
   }
 
   void view_orders(size_t user_id);
@@ -47,14 +46,14 @@ public:
 
   std::string get_info(size_t user_id);
 
-  std::vector<nlohmann::json> &get_to_send(size_t user_id);
+  // std::vector<nlohmann::json> &get_to_send(size_t user_id);
 
   size_t reg();
 
 private:
   OrderBook order_book_;
   std::unordered_map<size_t, UserInfo> users_;
-  std::unordered_map<size_t, std::vector<nlohmann::json>> to_send_;
+  // std::unordered_map<size_t, std::vector<nlohmann::json>> to_send_;
   size_t last_user_id_ = 0;
-  std::unordered_set<size_t> to_notify_;
+  // std::unordered_set<size_t> to_notify_;
 };
