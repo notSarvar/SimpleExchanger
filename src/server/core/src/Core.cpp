a8f1efc88241bad39e016a60f02d7404111bfedc
Sarvar Yuldashev <sarvar010104@gmail.com>
2024-10-18 15:49:40 +0300

    Final fixes

--- src/server/core/src/Core.cpp
+++ src/server/core/src/Core.cpp
@@ -1,35 +1,6 @@
 #include "Core.h"
 #include <cstddef>
 
-template <ESide side, typename... Args>
-std::unordered_set<size_t> &Core::make_order(Args &&...args) {
-  auto trade_handler = [this](size_t our_order_id, size_t other_order_id,
-                              size_t quantity) {
-    const Order &our_order = order_book_.get_order_by_id(our_order_id);
-    const Order &other_order = order_book_.get_order_by_id(other_order_id);
-    to_send_[other_order.user_id].push_back(other_order.as_json());
-    if (our_order.user_id != other_order.user_id) {
-      to_notify_.insert(other_order.user_id);
-      if constexpr (side == ESide::ESell) {
-        users_[other_order.user_id].add_usd(quantity);
-        users_[other_order.user_id].add_rub(-quantity * other_order.price);
-        users_[our_order.user_id].add_usd(-quantity);
-        users_[our_order.user_id].add_rub(quantity * other_order.price);
-      } else {
-        users_[other_order.user_id].add_usd(-quantity);
-        users_[other_order.user_id].add_rub(quantity * other_order.price);
-        users_[our_order.user_id].add_usd(quantity);
-        users_[our_order.user_id].add_rub(-quantity * other_order.price);
-      }
-    }
-  };
-  size_t order_id =
-      order_book_.addOrder(side, trade_handler, std::forward<Args>(args)...);
-  const Order &order = order_book_.get_order_by_id(order_id);
-  users_[order.user_id].add_order(order_id);
-  return to_notify_;
-}
-
 std::vector<nlohmann::json> &Core::get_to_send(size_t user_id) {
   return to_send_[user_id];
 }
